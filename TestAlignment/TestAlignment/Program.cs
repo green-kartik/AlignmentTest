8dff9ba0b6c3585a86cfd1438865516b4f7cc254
kartik jain <kartik.jain@safefleet.net>
2023-03-27 12:39:29 +0530

    first commit

---
+++ TestAlignment/TestAlignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestAlignment
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+	public class Point
+	{
+		public int x;
+		public int y;
+		public bool DoesContainDuplicate(Point[] p)
+		{
+			return true;
+		}
+	}
+}
